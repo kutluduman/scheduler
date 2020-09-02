758ca4adb5333cbfa647ffd6eafb1c4cdecaec94
kutluduman <kutluduman@gmail.com>
2020-09-01 20:30:48 -0400

    useState for the interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,8 @@ import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
   const [name,setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -14,19 +16,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={name}
+            onChange={event => setName(event.target.value)}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          value={name}
-           onChange={event => setName(event.target.value)}
+          value={interviewer}
+           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
