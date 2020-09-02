cea0c0e0723216456a10f956e7488af4a16cd267
kutluduman <kutluduman@gmail.com>
2020-09-02 18:22:06 -0400

    implemented the selectors function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,19 @@
 export const getAppointmentsForDay = (state, day) => {
+  const dayObj = state.days.find(element => element.name === day);
 
+  if (!dayObj) {
+    return [];
+  }
+
+  const appointmentIds = dayObj.appointments;
+
+  const appointmentsForDay = [];
+
+  for(const id in state.appointments) {
+    if (appointmentIds.includes(Number(id))) {
+      appointmentsForDay.push(state.appointments[id])
+    }
+  }
+
+  return appointmentsForDay;
 } 
