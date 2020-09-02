46085f103c600d7b4f3a57c905bda8234635c3f6
kutluduman <kutluduman@gmail.com>
2020-09-01 22:52:22 -0400

    Update the Appointment component to pass the student and interviewer props to the Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-import Header from "./Header"
-import Show from "./Show"
-import Empty from "./Empty"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./style.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-
-      <Header time={props.time}/>
-      {props.interview ? <Show/> : <Empty/>}
-
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
     </article>
-  )
-} 
+  );
+}
