7ebbc3f304f508fcb110240a5c351a1bbe34f6b4
kutluduman <kutluduman@gmail.com>
2020-09-01 22:42:23 -0400

    Imported header, show and empty. Also header was added that will show or empty according to interview existence

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
+
 import "./style.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
+
+      <Header time={props.time}/>
+      {props.interview ? <Show/> : <Empty/>}
 
     </article>
   )
