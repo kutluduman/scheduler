c07fe78924b32821039b4e228a66d21d2805112b
kutluduman <kutluduman@gmail.com>
2020-09-03 13:06:32 -0400

    Update the state object to include an interviewers object.

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,7 +14,8 @@ export default function Application(props) {
   const[state, setState] = useState({
     day:'Monday',
     days:[],
-    appointments:{}
+    appointments:{},
+    interviewers:{}
   });
 
   const setDay = day => setState(state => ({...state, day}));
@@ -23,9 +24,10 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(all => {
-      setState(state => ({...state, days: all[0].data, appointments: all[1].data}));
+      setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
   }, []);
   
