aabf30152a09e489becc50643f6d03b74e53ef8c
kutluduman <kutluduman@gmail.com>
2020-09-07 12:29:59 -0400

    Installed prop-type and imported and implemented into interviewlist component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+
+
 
 export default function InterviewerList(props) {
   return (
@@ -23,3 +26,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
