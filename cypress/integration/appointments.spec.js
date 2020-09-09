87a343345075fd4ccb3294e174bb4e236bc4e045
kutluduman <kutluduman@gmail.com>
2020-09-09 14:27:13 -0400

    cancel interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,9 +6,7 @@ describe("Appointments", () => {
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+    cy.get("[alt=Add]").first().click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
@@ -18,7 +16,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-  
+
   it("should edit an interview", () => {
     cy.get("[alt=Edit]").click({ force: true });
 
@@ -32,5 +30,14 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
